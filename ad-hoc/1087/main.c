9cb1c0eccc14f3d9deaf01bb36e986b3dc2a13cd
leandro <olegon.main@gmail.com>
2016-06-13 16:05:59 -0300

    Ad-hoc: 1087.

---
+++ ad-hoc/1087/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define ABS(n) ((n) < 0 ? -(n) : (n))
+
+int main (void) {
+    int x1, y1,
+        x2, y2;
+
+    while (scanf("%d %d %d %d", &x1, &y1, &x2, &y2), x1 != 0 || y1 != 0 || x2 != 0 || y2 != 0) {
+        // Mesma posição
+        if (x1 == x2 && y1 == y2) {
+            printf("0\n");
+        }
+
+        // Mesma linha / Mesma coluna
+        else if (x1 == x2 || y1 == y2) {
+            printf("1\n");
+        }
+
+        // Diagonal
+        else if (ABS(x1 - x2) == ABS(y1 - y2)) {
+            printf("1\n");
+        }
+
+        // Outro
+        else {
+            printf("2\n");
+        }
+    }
+
+    return 0;
+}
