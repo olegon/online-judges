bb3c3fbb782f074893a8bdb978fa855a8a1a599d
leandro <olegon.main@gmail.com>
2016-06-13 15:11:59 -0300

    - Matemática: 1161.

---
+++ matemática/1161/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+
+unsigned long long int cache[] = { 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600, 6227020800, 87178291200, 1307674368000, 20922789888000, 355687428096000, 6402373705728000, 121645100408832000, 2432902008176640000 };
+
+unsigned long long int fatorial(int n);
+
+int main (void) {
+    int a, b;
+
+    while (scanf("%d %d\n", &a, &b) != EOF) {
+        printf("%llu\n", fatorial(a) + fatorial(b));
+    }
+
+    return 0;
+}
+
+unsigned long long int fatorial(int n) {
+    return cache[n];
+}
