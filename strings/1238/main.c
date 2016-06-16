03ec1687db1add3eb95d548ed26acb0a29c086cc
leandro <olegon.main@gmail.com>
2016-06-16 15:44:25 -0300

    - Strings: 1238.

---
+++ strings/1238/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#define STRING_SIZE 64
+
+
+int main (void) {
+    int     casos;
+
+    char    a[STRING_SIZE],
+            b[STRING_SIZE];
+
+    char    *pa,
+            *pb;
+
+    scanf("%d\n", &casos);
+
+    while (casos-- > 0) {
+        scanf("%s %s\n", a, b);
+
+        pa = a;
+        pb = b;
+
+        while (*pa != '\0' && *pb != '\0') {
+            putc(*pa, stdout);
+            putc(*pb, stdout);
+
+            pa++;
+            pb++;
+        }
+
+        while (*pa != '\0') {
+            putc(*pa, stdout);
+            pa++;
+        }
+
+        while (*pb != '\0') {
+            putc(*pb, stdout);
+            pb++;
+        }
+
+        putc('\n', stdout);
+    }
+
+    return 0;
+}
