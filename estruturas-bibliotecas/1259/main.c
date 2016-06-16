b3c5a41aa2aba776156c9a79d0ddbb73e566baad
leandro <olegon.main@gmail.com>
2016-06-16 16:14:41 -0300

    - Estruturas-bibliotecas: 1259.

---
+++ estruturas-bibliotecas/1259/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 100000
+
+int comp(const void *a, const void *b);
+
+int main (void) {
+    int casos,
+        numeros[SIZE],
+        i;
+
+    scanf("%d\n", &casos);
+
+    for (i = 0; i < casos; i++) {
+        scanf("%d\n", &numeros[i]);
+    }
+
+    qsort(numeros, casos, sizeof(int), comp);
+
+    for (i = 0; i < casos; i++) {
+        printf("%d\n", numeros[i]);
+    }
+
+    return 0;
+}
+
+int comp(const void *a, const void *b) {
+    int ia,
+        ib;
+
+    ia = *(int*)a;
+    ib = *(int*)b;
+
+    if (ia % 2 == 0 && ib % 2 == 0) {
+        return ia - ib;
+    }
+    else if (ia % 2 == 0) {
+        return -1;
+    }
+    else if (ib % 2 == 0) {
+        return 1;
+    }
+    else {
+        return ib - ia;
+    }
+}
