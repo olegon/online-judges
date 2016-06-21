15b5122f114ad437644be332a5eb3db9f005d24d
leandro <olegon.main@gmail.com>
2016-06-21 12:59:17 -0300

    - Strings: 1235.

---
+++ strings/1235/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+#define STRING_SIZE 128
+
+void imprimirCorretamente(const char *str);
+
+int main (void) {
+    int     casos;
+    char    str[STRING_SIZE];
+
+    scanf("%d", &casos);
+    gets(str);
+
+    while (casos-- > 0) {
+        fgets(str, STRING_SIZE, stdin);
+
+        imprimirCorretamente(str);
+    }
+
+    return 0;
+}
+
+void imprimirCorretamente(const char *str) {
+    int     length,
+            half,
+            i;
+
+    // -1 para desconsiderar o \n
+    length = strlen(str) - 1;
+    half = length / 2;
+
+    for (i = half - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+
+    for (i = length - 1; i >= half; i--) {
+        printf("%c", str[i]);
+    }
+
+    printf("\n");
+}
