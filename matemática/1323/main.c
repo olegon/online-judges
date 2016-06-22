8d8fb39572d82addccf45dc7f4bf9bd83908a448
leandro <olegon.main@gmail.com>
2016-06-22 17:22:49 -0300

    - Matemática: 1323.

---
+++ matemática/1323/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define SIZE 100
+
+void preencherCache(int *cache, int size);
+
+int main (void) {
+    int quadrados,
+        cache[SIZE + 1];
+
+    preencherCache(cache, SIZE);
+
+    while (scanf("%d", &quadrados), quadrados > 0) {
+        printf("%d\n", cache[quadrados]);
+    }
+
+    return 0;
+}
+
+void preencherCache(int *cache, int size) {
+    int i;
+
+    cache[1] = 1;
+    for (i = 2; i <= size; i++) {
+        cache[i] = i * i + cache[i - 1];
+    }
+}
