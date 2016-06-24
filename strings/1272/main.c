d0c0e6995711dc67feb63e2ce4b948b79bf467e6
leandro <olegon.main@gmail.com>
2016-06-24 12:08:48 -0300

    - Strings: 1272.

---
+++ strings/1272/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define ESPACO(c) ((c) == ' ')
+
+void imprimirMensagemOculpa(const char *linha);
+
+#define TAMANHO_DA_LINHA 64
+
+int main (void) {
+    int     casos;
+    char    linha[TAMANHO_DA_LINHA];
+
+    scanf("%d\n", &casos);
+
+    while (casos-- > 0) {
+        fgets(linha, TAMANHO_DA_LINHA, stdin);
+        // printf("%s", linha);
+        imprimirMensagemOculpa(linha);
+    }
+
+    return 0;
+}
+
+void imprimirMensagemOculpa(const char *linha) {
+    int lendoPalavra = 0;
+
+    while (*linha != '\n') {
+        if (ESPACO(*linha)) {
+            lendoPalavra = 0;
+        }
+        else if (!lendoPalavra) {
+            putc(*linha, stdout);
+            lendoPalavra = 1;
+        }
+
+        linha++;
+    }
+
+    putc('\n', stdout);
+}
