b50ec440f577517128dfdc2626562752c516533f
leandro <olegon.main@gmail.com>
2016-06-27 15:34:16 -0300

    - Matemática: 1436.

---
+++ matemática/1436/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main (void) {
+    int caso,
+        casos,
+        quantidade,
+        i,
+        indice,
+        idade;
+
+    scanf("%d", &casos);
+
+    for (caso = 1; caso <= casos; caso++) {
+        scanf("%d", &quantidade);
+
+        indice = quantidade / 2;
+
+        for (i = 0; i < quantidade; i++) {
+            if (i == indice) {
+                scanf("%d", &idade);
+            }
+            else {
+                scanf("%*d");
+            }
+        }
+
+        printf("Case %d: %d\n", caso, idade);
+    }
+
+    return 0;
+}
