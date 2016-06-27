59ebb256df2f6648c7e0a1fd2bb44759e3200423
leandro <olegon.main@gmail.com>
2016-06-27 13:03:13 -0300

    - [FIXED] Ad-hoc: 1542.

--- ad-hoc/1542/main.c
+++ ad-hoc/1542/main.c
@@ -5,8 +5,19 @@ int main (void) {
             quantidadeDeDias,
             quantidadeDePaginas;
 
+    int total;
+
     while (scanf("%lf %lf %lf\n", &quantidadeDePaginasLidas, &quantidadeDeDias, &quantidadeDePaginas) == 3) {
-        printf("%d paginas\n", (int)((quantidadeDePaginasLidas * quantidadeDeDias) / (quantidadeDePaginasLidas - quantidadeDePaginas) * quantidadeDePaginas * - 1.0));
+
+        total = (quantidadeDePaginasLidas * quantidadeDeDias) / (quantidadeDePaginasLidas - quantidadeDePaginas) * quantidadeDePaginas * - 1.0;
+
+        if (total == 1) {
+            printf("1 pagina\n");
+        }
+        else {
+            printf("%d paginas\n", total);
+        }
+
     }
 
     return 0;
