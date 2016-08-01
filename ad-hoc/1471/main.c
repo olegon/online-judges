8df26d754e666dccb57be458147c63211a1f9859
leandro <olegon.main@gmail.com>
2016-08-01 17:32:31 -0300

    - Ad-hoc: 1471.

---
+++ ad-hoc/1471/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+#define QUANTIDADE_MERGULHADORES 10001
+
+int main (void) {
+    int foram, voltaram,
+        mergulhadoresQueVoltaram[QUANTIDADE_MERGULHADORES],
+        i,
+        mergulhadorQueVoltou;
+
+    for (i = 0; i < QUANTIDADE_MERGULHADORES; i++) {
+        mergulhadoresQueVoltaram[i] = 0;
+    }
+
+    while (scanf("%d %d", &foram, &voltaram) == 2) {
+        if (foram == voltaram) {
+            // clear
+            for (i = 1; i <= voltaram; i++) { scanf("%d", &mergulhadorQueVoltou); }
+
+            printf("*\n");
+        }
+        else {
+            for (i = 1; i <= voltaram; i++) {
+                scanf("%d", &mergulhadorQueVoltou);
+
+                printf("DEBUG: %d\n", mergulhadorQueVoltou);
+
+                // set
+                mergulhadoresQueVoltaram[mergulhadorQueVoltou] = 1;
+            }
+
+            for (i = 1; i <= foram; i++) {
+                if (!mergulhadoresQueVoltaram[i]) {
+                    printf("%d ", i);
+                }
+
+                // reset
+                mergulhadoresQueVoltaram[i] = 0;
+            }
+
+            printf("\b\n\b");
+        }
+    }
+
+    return 0;
+}
