58071dc572344b63058427ccaeb6ac93ce04c8fa
leandro <olegon.main@gmail.com>
2016-08-19 16:40:31 -0300

    - Strings: 1257.

---
+++ strings/1257/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#define POSICAO_DA_LETRA(c) ((c) - 'A')
+
+#define MAX_STRING_SIZE 64
+
+int main (void) {
+    int     casos,
+            quantidadeDeLinhas,
+            indiceDaLinha,
+            indiceDaPalavra,
+            hash;
+
+    char    palavra[MAX_STRING_SIZE];
+
+    scanf("%d\n", &casos);
+
+    while (casos-- > 0) {
+        hash = 0;
+
+        scanf("%d\n", &quantidadeDeLinhas);
+
+        for (indiceDaLinha = 0; indiceDaLinha < quantidadeDeLinhas; indiceDaLinha++) {
+            scanf("%s\n", palavra);
+
+            indiceDaPalavra = 0;
+            while (palavra[indiceDaPalavra] != '\0') {
+                hash += POSICAO_DA_LETRA(palavra[indiceDaPalavra]) + indiceDaLinha + indiceDaPalavra;
+
+                indiceDaPalavra++;
+            }
+        }
+
+        printf("%d\n", hash);
+    }
+
+    return 0;
+}
