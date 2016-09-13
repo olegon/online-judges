92a55a974e2388d227c3249fe5588c94f0750a1e
leandro <olegon.main@gmail.com>
2016-09-13 09:29:08 -0300

    Maratona SBC 2016
    - 2233 (ad-hoc, etiquetas coloridas)

---
+++ origens/maratona-sbc-2016/2233-etiquetas-coloridas/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (void) {
+    int ladoDoVermelho, ladoDoVerde, ladoDoAzul;
+    long long int quantidadeDeVermelho, quantidadeDeVerde, quantidadeDeAzul;
+
+    scanf("%x %x %x", &ladoDoVermelho, &ladoDoVerde, &ladoDoAzul);
+
+    quantidadeDeVermelho = 1;
+
+    quantidadeDeVerde = ladoDoVermelho / ladoDoVerde;
+    quantidadeDeVerde *= quantidadeDeVerde;
+
+    quantidadeDeAzul = ladoDoVerde / ladoDoAzul;
+    quantidadeDeAzul *= quantidadeDeAzul;
+    quantidadeDeAzul *= quantidadeDeVerde;
+
+    printf("%llx\n", quantidadeDeVermelho + quantidadeDeVerde + quantidadeDeAzul);
+
+    return 0;
+}
