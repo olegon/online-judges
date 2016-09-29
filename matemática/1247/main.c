2c098773d64b2195813d1a05e6ebc2ab7a8f538c
leandro <olegon.main@gmail.com>
2016-09-29 17:13:00 -0300

    Problema Resolvido
    - Matemática: 1247.

---
+++ matemática/1247/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (void) {
+    int D, VF, VG;
+
+    while (scanf("%d %d %d\n", &D, &VF, &VG) != EOF) {
+        double tF = 12.0 / VF;
+        double tG = sqrt(pow(D, 2) + pow(12, 2)) / VG;
+
+        if (tG <= tF) {
+            printf("S\n");
+        }
+        else {
+            printf("N\n");
+        }
+    }
+
+    return 0;
+}
