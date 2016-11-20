0176bf4a0db72ea669cbacf77cabe11f47638151
Leandro Oliveira <olegon.main@gmail.com>
2016-11-19 23:57:09 -0200

    Ad-hoc: 2349.

---
+++ ad-hoc/2349/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+
+    int N, C, S;
+    int count = 0;
+
+    cin >> N >> C >> S;
+
+    int pos = 1;
+
+    if (pos == S) count++;
+
+    for (int i = 0; i < C; i++) {
+        int command;
+
+        cin >> command;
+
+        pos += command;
+
+        if (pos > N) pos = 1;
+        else if (pos < 1) pos = N;
+
+        if (pos == S) count++;
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
