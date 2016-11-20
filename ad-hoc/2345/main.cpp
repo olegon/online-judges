18a8e91cd845a6ef398d5dfb59b0d20ebc4a0079
Leandro Oliveira <olegon.main@gmail.com>
2016-11-19 22:59:56 -0200

    Ad-hoc: 2345.

---
+++ ad-hoc/2345/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+
+    vector<int> v;
+
+    for (int i = 0; i < 4; i++) {
+        int s;
+
+        cin >> s;
+
+        v.push_back(s);
+    }
+
+    sort(v.begin(), v.end());
+
+    cout << abs((v[0] + v[3]) - (v[1] + v[2])) << endl;
+
+    return 0;
+}
