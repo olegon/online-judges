81ae7939dd1ea2f1b34f058fd2bc615a50805777
leandro <olegon.main@gmail.com>
2016-11-22 10:47:19 -0300

    Estruturas e bibliotecas: 1256.

---
+++ estruturas-bibliotecas/1256/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+
+    int N;
+
+    cin >> N;
+
+    while (N-- > 0) {
+        int M, C;
+
+        cin >> M >> C;
+
+        vector<int> table[M];
+
+        while (C-- > 0) {
+            int X;
+
+            cin >> X;
+
+            table[X % M].push_back(X);
+        }
+
+        for (int i = 0; i < M; i++) {
+            cout << i << " -> ";
+
+            for (size_t j = 0; j < table[i].size(); j++) {
+                cout << table[i][j] << " -> ";
+            }
+
+            cout << "\\\n";
+        }
+
+        if (N > 0) cout << '\n';
+    }
+
+    return 0;
+}
