c7e0139dc0ff4b1d8b1dbc3c40b3a12eed5b558c
leandro <olegon.main@gmail.com>
2016-11-23 12:34:45 -0300

    Matemática: 2149

---
+++ matemática/2149/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+#define MAX_NUM 17
+
+unsigned long long int sequencia[MAX_NUM + 1];
+
+int main(void) {
+    ios::sync_with_stdio(false);
+
+    sequencia[1] = 0;
+    sequencia[2] = 1;
+    sequencia[3] = 1;
+
+    // DP
+    for (int i = 4; i <= MAX_NUM; i++) {
+        if (i % 2 == 0) {
+            sequencia[i] = sequencia[i - 2] * sequencia[i - 1];
+        }
+        else {
+            sequencia[i] = sequencia[i - 2] + sequencia[i - 1];
+        }
+    }
+
+    int N;
+
+    while (cin >> N) {
+        cout << sequencia[N] << '\n';
+    }
+
+    return 0;
+}
