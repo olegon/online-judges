52ca8734f8396ad1f503c4f926d5d14a270e3713
leandro <olegon.main@gmail.com>
2016-11-24 10:29:24 -0300

    Matemática: 1795.

---
+++ matemática/1795/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+typedef unsigned long long int uint64;
+
+uint64 pow(uint64 base, int expoent);
+
+int main(void) {
+    ios::sync_with_stdio(false);
+
+    uint64 N;
+
+    cin >> N;
+
+    cout << pow(3, N) << '\n';
+
+    return 0;
+}
+
+uint64 pow(uint64 base, int expoent) {
+    if (expoent == 0) {
+        return 1;
+    }
+    else {
+        int half_exporent = expoent / 2;
+
+        uint64 result = pow(base, half_exporent) * pow(base, half_exporent);
+
+        if (expoent % 2 == 0) return result;
+        else return result * base;
+    }
+}
