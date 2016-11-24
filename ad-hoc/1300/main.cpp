b204d16aefa16973a70e56e7fd35b04faa0ac9d7
leandro <olegon.main@gmail.com>
2016-11-24 11:04:44 -0300

    Ad-hoc: 1300

---
+++ ad-hoc/1300/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+bool eh_possivel(int A);
+
+int main(void) {
+    ios::sync_with_stdio(false);
+
+    int A;
+
+    while (cin >> A) {
+        if (eh_possivel(A)) cout << "Y\n";
+        else cout << "N\n";
+    }
+
+    return 0;
+}
+
+bool eh_possivel(int A) {
+    int r = A % 6;
+
+    return (r == 0) ;
+}
