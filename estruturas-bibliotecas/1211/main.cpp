94a12d01c139cc10cc2d7df4fb5ae5a878918544
leandro <olegon.main@gmail.com>
2016-11-24 12:43:38 -0300

    Estruturas e bibliotecas: 1211.

---
+++ estruturas-bibliotecas/1211/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+
+    int N;
+    while (cin >> N) {
+        cin.ignore(1);
+
+        vector<string> numeros;
+
+        for (int i = 0; i < N; i++) {
+            string numero;
+            getline(cin, numero);
+            numeros.push_back(numero);
+        }
+
+        sort(numeros.begin(), numeros.end());
+
+        int count = 0;
+
+        for (int i = 1; i < N; i++) {
+            for (size_t j = 0;
+                j < numeros[i].length()
+                && numeros[i - 1][j] == numeros[i][j]
+                && (j == 0 || numeros[i - 1][j - 1] == numeros[i][j - 1]);
+                j++) {
+                count++;
+            }
+        }
+
+        cout << count << '\n';
+    }
+
+    return 0;
+}
