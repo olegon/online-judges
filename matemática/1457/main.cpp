69652f4d485217337b8fa6f028b70d9f35a044a3
leandro <olegon.main@gmail.com>
2016-11-24 15:47:34 -0300

    Matemática: 1457

---
+++ matemática/1457/main.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+
+using namespace std;
+
+typedef unsigned long long int uint64;
+
+uint64 k_fat(int N, int K);
+
+int main(void) {
+    int T;
+
+    scanf("%d\n", &T);
+
+    while (T-- > 0) {
+        int N, K = 0;
+        char C;
+
+        scanf("%d", &N);
+
+        while (scanf("%c", &C), C == '!') K++;
+
+        printf("%llu\n", k_fat(N, K));
+    }
+
+    return 0;
+}
+
+uint64 k_fat(int N, int K) {
+    if (N < 2) return 1;
+    else return N * k_fat(N - K, K);
+}
