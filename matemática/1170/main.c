91d911e5a57ed52159e68edf53c48a1fb24c48cc
leandro <olegon.main@gmail.com>
2016-12-21 11:31:38 -0300

    Matemática: 1170
    - Refatoração.

--- matemática/1170/main.c
+++ matemática/1170/main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <math.h>
 
-int dias (double dias);
-
 int main (void) {
     int     casos;
     double  comidaDisponivel;
@@ -12,48 +10,8 @@ int main (void) {
     while (casos-- > 0) {
         scanf("%lf", &comidaDisponivel);
 
-        // printf("%.0f dias\n", floor(log(comidaDisponivel) / log(2)));
-        printf("%d dias\n", dias(comidaDisponivel));
+        printf("%.0f dias\n", ceil(log2(comidaDisponivel)));
     }
 
     return 0;
-}
-
-int dias (double dias) {
-    if (dias <= 1.0) {
-        return 0;
-    }
-    else if (dias <= 2.0) {
-        return 1;
-    }
-    else if (dias <= 4.0) {
-        return 2;
-    }
-    else if (dias <= 8.0) {
-        return 3;
-    }
-    else if (dias <= 16.0) {
-        return 4;
-    }
-    else if (dias <= 32.0) {
-        return 5;
-    }
-    else if (dias <= 64.0) {
-        return 6;
-    }
-    else if (dias <= 128.0) {
-        return 7;
-    }
-    else if (dias <= 256.0) {
-        return 8;
-    }
-    else if (dias <= 512.0) {
-        return 9;
-    }
-    else if (dias <= 1024.0) {
-        return 10;
-    }
-    else {
-        return -1;
-    }
 }
