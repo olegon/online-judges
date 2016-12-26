31271a2e26c8f3e5a0fe7b256d756a8e4ca5c718
leandro <olegon.main@gmail.com>
2016-12-26 16:35:48 -0300

    Grafos: 1076.

---
+++ grafos/1076/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+#define MAX_N 49
+
+using namespace std;
+
+void reset(bool mat[MAX_N][MAX_N]);
+int get_i(int n, int V);
+int get_j(int n, int V);
+
+int main(void) {
+    ios::sync_with_stdio(false);
+
+    int T;
+
+    cin >> T;
+
+    while (T-- > 0) {
+        int N;
+
+        cin >> N;
+
+        int V, A;
+
+        cin >> V >> A;
+
+        bool mat[MAX_N][MAX_N];
+        reset(mat);
+
+        int count = 0;
+
+        for (int i = 0; i < A; i++) {
+            int a, b;
+
+            cin >> a >> b;
+
+            if (mat[b][a] || mat[a][b]) continue;
+
+            mat[a][b] = true;
+            count++;
+        }
+
+        cout << (2 * count) << '\n';
+    }
+
+    return 0;
+}
+
+void reset(bool mat[MAX_N][MAX_N]) {
+    for (size_t i = 0; i < MAX_N; i++) {
+        for (size_t j = 0; j < MAX_N; j++) {
+            mat[i][j] = false;
+        }
+    }
+}
