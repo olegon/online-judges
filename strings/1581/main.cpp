ab98f665f70748da05d5e56f70a47c2d3790abbe
leandro <olegon.main@gmail.com>
2016-12-26 16:52:38 -0300

    Strings: 1581.

---
+++ strings/1581/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+
+    int N;
+
+    cin >> N;
+
+    while (N-- > 0) {
+        int K;
+        string idioma_final;
+
+        cin >> K;
+        cin.ignore(1);
+
+        getline(cin, idioma_final);
+
+        for (int i = 1; i < K; i++) {
+            string idioma;
+
+            getline(cin, idioma);
+
+            if (idioma != idioma_final) idioma_final = "ingles";
+        }
+
+        cout << idioma_final << '\n';
+    }
+
+    return 0;
+}
