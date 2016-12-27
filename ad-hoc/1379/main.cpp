4b48e2b2a07e2754665a44d2c18f9b772927a307
leandro <olegon.main@gmail.com>
2016-12-27 11:26:33 -0300

    Ad-hoc: 1379

---
+++ ad-hoc/1379/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+
+    int A, B;
+
+    while (cin >> A >> B, A != 0 || B != 0) {
+        if (A > B) {
+            int c = A;
+            A = B;
+            B = c;
+        }
+
+        int C = 3 * A - A - B;
+
+        cout << C << endl;
+    }
+
+    return 0;
+}
