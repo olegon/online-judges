a2bf8acdfd000f19a5676ce916da7659d3d91eda
leandro <olegon.main@gmail.com>
2016-12-27 15:12:52 -0300

    2146 (Accepted)

---
+++ 2146/main.cpp
@@ -0,0 +1,20 @@
+/*
+Senha
+https://www.urionlinejudge.com.br/judge/pt/problems/view/2146
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+
+    int N;
+
+    while (cin >> N) {
+        cout << (N - 1) << '\n';        
+    }
+
+    return 0;
+}
