83e1993ac515407565c8d729dbeda7e5f28d5a9d
olegon <olegon.main@gmail.com>
2017-03-29 14:41:16 -0300

    URI Online Judge - 1228
    - Grid de Largada;
    - Accepted.

---
+++ 1228/main.cpp
@@ -0,0 +1,56 @@
+/*
+Grid de Largada
+https://www.urionlinejudge.com.br/judge/pt/problems/view/1228
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+
+    int N;
+
+    while (cin >> N) {
+        vector<int> largada(N);
+        vector<int> chegada(N);
+        int ultrapassagens = 0;
+
+        for (int i = 0; i < N; i++) {
+            int carro;
+
+            cin >> carro;
+
+            largada[carro - 1] = i + 1;
+        }
+
+        for (int i = 0; i < N; i++) {
+            int carro;
+
+            cin >> carro;
+
+            chegada[i] = carro;
+        }
+
+        for (int i = 1; i < N; i++) {
+            int carroA = chegada[i - 1];
+            int carroB = chegada[i];
+            int diff = largada[carroA - 1] - largada[carroB - 1];
+
+            // cout << carroA << " -> " << carroB << endl;
+            // cout << largada[carroA - 1] << " -> " << largada[carroB - 1] << endl;
+            // cout << diff << endl;
+            // cout << endl << endl;
+
+            if (diff > 0) ultrapassagens += diff;
+        }
+
+        cout << ultrapassagens << '\n';
+    }
+
+    return EXIT_SUCCESS;
+}
