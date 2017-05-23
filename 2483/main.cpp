25c27762856c96dbbe95da40d39c6f0636215027
olegon <olegon.main@gmail.com>
2017-05-23 13:38:50 -0300

    URI Online Judge - 2483
    - Feliz Nataaal!
    - Accepted.

---
+++ 2483/main.cpp
@@ -0,0 +1,25 @@
+/*
+Feliz Nataaal!
+https://www.urionlinejudge.com.br/judge/pt/problems/view/2483
+*/
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+
+    int N;
+
+    cin >> N;
+
+    cout << "Feliz nat";
+
+    while (N-- > 0) cout << 'a';
+
+    cout << "l!" << endl;
+    
+    return EXIT_SUCCESS;
+}
