c2102bd36bd90bc4d0f5663322254a9b27770326
olegon <olegon.main@gmail.com>
2017-09-17 17:45:50 -0300

    URI 2661 - Despojados
    - 20% WA

--- uri-online-judge/2661/main.cpp
+++ uri-online-judge/2661/main.cpp
@@ -11,8 +11,8 @@ using namespace std;
 
 typedef unsigned long long int uint64;
 
-bool despojado(uint64 n);
-bool primo(uint64 n);
+vector<uint64> primosAte(uint64 n);
+bool despojado(uint64 n, vector<uint64> &primos);
 
 int main(void) {
     ios::sync_with_stdio(false);
@@ -21,59 +21,58 @@ int main(void) {
 
     cin >> N;
 
-    if (primo(N)) {
-        cout << 0 << endl;
-    }
-    else {
-        uint64 limit = sqrt(N);
-        
-        if (despojado(N)) r++;
-        for (uint64 i = 2; i <= limit; i++) {
-            if (N % i == 0) {
-                uint64 a = i, b = N / i;
+    vector<uint64> primos = primosAte(N);
+    uint64 limit = sqrt(N);
     
-                if (despojado(a)) r++;
-                if (b != a && despojado(b)) r++;
-            }
+    if (despojado(N, primos)) r++;
+    for (uint64 i = 2; i <= limit; i++) {
+        if (N % i == 0) {
+            uint64 a = i, b = N / i;
+
+            if (despojado(a, primos)) r++;
+            if (b != a && despojado(b, primos)) r++;
         }
-    
-        cout << r << endl;
     }
+
+    cout << r << endl;
     
     return EXIT_SUCCESS;
 }
 
-bool primo(uint64 n) {
-    if (n < 2) return false;
-    if (n % 2 == 0) return false;
+vector<uint64> primosAte(uint64 n) {
+    vector<uint64> primos;
 
+    if (n < 2) return primos;
+
+    primos.push_back(2);
+    
     uint64 limit = sqrt(n);
-
     for (uint64 i = 3; i <= limit; i += 2) {
-        if (n % i == 0) return false;
+        bool _primo = true;
+        uint64 _limit = sqrt(i);
+
+        for (uint64 p : primos) {
+            if (!_primo || p > _limit) break;
+            
+            _primo = i % p != 0;
+        }
+        
+        if (_primo) primos.push_back(i);
     }
 
-    return true;
+    return primos;
 }
 
-bool despojado(uint64 n) {
+bool despojado(uint64 n, vector<uint64> &primos) {
     uint64 ultimoDivisor = 0, divisores = 0;
     
-    while (n % 2 == 0) {
-        if (ultimoDivisor == 2) return false;
-        
-        n /= 2;
-        divisores++;
-        ultimoDivisor = 2;
-    }
-    
-    for (uint64 i = 3;  n != 1; i += 2) {
-        while (n % i == 0) {
-            if (ultimoDivisor == i) return false;
+    for (auto p = primos.begin(); n > 1 && p != primos.end(); ++p) {
+        while (n % *p == 0) {
+            if (ultimoDivisor == *p) return false;
 
-            n /= i;
+            n /= *p;
+            ultimoDivisor = *p;
             divisores++;
-            ultimoDivisor = i;
         }
     }
 
