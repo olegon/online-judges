8792900ec0d09565603124323edb41890b9ba0b5
olegon <olegon.main@gmail.com>
2018-09-24 22:08:10 -0300

    Hacker Rank - Abbreviation (Dynamic Programming)
    - Accepted (2nd try);

--- hacker-rank/abbr/main.cpp
+++ hacker-rank/abbr/main.cpp
@@ -7,42 +7,58 @@ https://www.hackerrank.com/challenges/abbr/problem
 
 using namespace std;
 
+enum state {
+    NONE = 0,
+    OK = 1,
+    NOK = 2
+};
 
-bool canTransform(const string &a, const string &b, size_t ia, size_t ib) {
+
+bool canTransform(const string &a, const string &b, size_t ia, size_t ib, vector< vector<state> > &dp) {
     // cout << ia << " " << ib << endl;
 
-    // Se as duas string foram até o final
-    if (ia >= a.length() && ib >= b.length()) {
-        return true;
+    if (dp[ia][ib] == NONE) {
+        bool result;
+
+        // Se as duas string foram até o final
+        if (ia >= a.length() && ib >= b.length()) {
+            result = true;
+        }
+        // Se somente a foi até o final
+        else if (ia >= a.length()) {
+            result = false;
+        }
+        // Se somente b foi até o final, não pode haver maiúscular em a.
+        else if (ib >= b.length()) {
+            result = islower(a[ia]) && canTransform(a, b, ia + 1, ib, dp);
+        }
+        // Se são iguais (a[ia] é maiúsculo), avançar a e b
+        else if (a[ia] == b[ib]) {
+            result = canTransform(a, b, ia + 1, ib + 1, dp);
+        }
+        // Se a[ia] for b[ib] minúscula, analisar obter e não obter.
+        else if (toupper(a[ia]) == b[ib]) {
+            result = canTransform(a, b, ia + 1, ib, dp) || canTransform(a, b, ia + 1, ib + 1, dp);
+        }
+        // Se a[ia] é maiúsculo e diferente de b[ib]
+        else if (isupper(a[ia])) {
+            result = false;
+        }
+        // Se a[ia] é minúsculo e diferente de b[ib]
+        else {
+            result = canTransform(a, b, ia + 1, ib, dp);
+        }
+
+        dp[ia][ib] = result ? OK : NOK;
     }
 
-    // Se somente a foi até o final
-    if (ia >= a.length()) {
-        return false;
-    }
+    return dp[ia][ib] == OK;
+}
 
-    // Se somente b foi até o final, não pode haver maiúscular em a.
-    if (ib >= b.length()) {
-        return islower(a[ia]) && canTransform(a, b, ia + 1, ib);
-    }
-    
-    // Se são iguais (a[ia] é maiúsculo), avançar a e b
-    if (a[ia] == b[ib]) {
-        return canTransform(a, b, ia + 1, ib + 1);
-    }
+bool canTransform(const string &a, const string &b) {
+    vector< vector<state> > dp(a.length() + 1, vector<state>(b.length() + 1, NONE));
 
-    // Se a[ia] for b[ib] minúscula, analisar obter e não obter.
-    if (toupper(a[ia]) == b[ib]) {
-        return canTransform(a, b, ia + 1, ib) || canTransform(a, b, ia + 1, ib + 1);
-    }
-
-    // Se a[ia] é maiúsculo e diferente de b[ib]
-    if (isupper(a[ia])) {
-        return false;
-    }
-
-    // Se a[ia] é minúsculo e diferente de b[ib]
-    return canTransform(a, b, ia + 1, ib);
+    return canTransform(a, b, 0, 0, dp);
 }
 
 int main(void) {
@@ -61,7 +77,7 @@ int main(void) {
         // cout << a << endl;
         // cout << b << endl;
 
-        if (canTransform(a, b, 0, 0)) cout << "YES";
+        if (canTransform(a, b)) cout << "YES";
         else cout << "NO";
 
         cout << endl;
