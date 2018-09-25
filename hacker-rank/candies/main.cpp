aa3c11515b9bfa7d7a21d99da4a7f111fc85c360
olegon <olegon.main@gmail.com>
2018-09-24 23:04:27 -0300

    Hacker Rank - Candies (WA)
    - Dynamic Programming;
    - 2 cases failing;

---
+++ hacker-rank/candies/main.cpp
@@ -0,0 +1,39 @@
+/*
+Candies
+https://www.hackerrank.com/challenges/candies/problem
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+
+    int N,
+        total = 0;
+
+    cin >> N;
+
+    vector<int> ratings(N), candies(N);
+
+    for (int i = 0; i < N; i++) cin >> ratings[i];
+
+    candies[0] = 1;
+    for (int i = 1; i < N; i++) {
+        if (ratings[i] > ratings[i - 1]) candies[i] = candies[i - 1] + 1;
+        else candies[i] = 1;
+    }
+
+    for (int i = N - 2; i > -1; i--) {
+        if (ratings[i] > ratings[i + 1]) candies[i] = max(candies[i + 1] + 1, candies[i]);
+    }
+
+    for (int i = 0; i < N; i++) {
+        total += candies[i];
+    }
+
+    cout << total << endl;
+
+    return EXIT_SUCCESS;
+}
