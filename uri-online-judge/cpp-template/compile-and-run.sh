3221a77b2d2fb3ab4af2b07fa8e31db8c8a52f87
Leandro Gonçalves de Oliveira <olegon.main@gmail.com>
2020-03-31 17:08:58 -0300

    Updating URI Online Judge cpp template

--- uri-online-judge/cpp-template/compile-and-run.sh
+++ uri-online-judge/cpp-template/compile-and-run.sh
@@ -2,5 +2,4 @@
 
 set -x
 
-g++ main.cpp -W -Wall -std=c++14 -O3 && for f in input*.txt; do cat "$f" | ./a.out; done;
-
+g++ main.cpp -W -Wall -std=c++11 -O2 -lm && for f in input*.txt; do cat "$f" | ./a.out; done;
