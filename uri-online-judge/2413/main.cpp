a81d790cf5c772a8c617f30585215ca109ae4c40
Leandro Gonçalves de Oliveira <olegon.main@gmail.com>
2020-03-31 17:22:11 -0300

    URI Online Judge - 2413
    - accepted;
    - ad-hoc;

---
+++ uri-online-judge/2413/main.cpp
@@ -0,0 +1,20 @@
+/*
+Busca na Internet
+https://www.urionlinejudge.com.br/judge/pt/problems/view/2413
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+
+    int T;
+    
+    cin >> T;
+
+    cout << T * 4 << endl;
+
+    return EXIT_SUCCESS;
+}
