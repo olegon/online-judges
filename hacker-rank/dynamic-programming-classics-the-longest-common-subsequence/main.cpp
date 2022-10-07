1a60bb26487dc3cdf2bd4f9c16d51300cc6015a4
Leandro Gonçalves de Oliveira <olegon.main@gmail.com>
2022-10-07 11:51:15 -0300

    HackerRank: The Longest Common Subsequence
    - adding recursive solution! (:

--- hacker-rank/dynamic-programming-classics-the-longest-common-subsequence/main.cpp
+++ hacker-rank/dynamic-programming-classics-the-longest-common-subsequence/main.cpp
@@ -1,27 +1,27 @@
 /*
 The Longest Common Subsequence
 https://www.hackerrank.com/challenges/dynamic-programming-classics-the-longest-common-subsequence/problem
+
+Studying Cormen's book!
 */
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int value(vector<vector<int>> &mat, int i, int j) {
+int lcs_bottom_up_value(const vector<vector<int>> &mat, int i, int j) {
     if (i < 0 || j < 0) return 0;
 
     return mat[i][j];
 }
 
-vector<int> lcs(vector<int> a, vector<int> b) {
-    
-
+vector<int> lcs_bottom_up(vector<int> &a, vector<int> &b) {
     auto mat = vector<vector<int>>(a.size(), vector<int>(b.size()));
 
     for (size_t i = 0; i < a.size(); i++) {
         for (size_t j = 0; j < b.size(); j++) {
-            if (a[i] == b[j]) mat[i][j] = value(mat, i - 1, j - 1) + 1;
-            else mat[i][j] = max(value(mat, i - 1, j), value(mat, i, j - 1));
+            if (a[i] == b[j]) mat[i][j] = lcs_bottom_up_value(mat, i - 1, j - 1) + 1;
+            else mat[i][j] = max(lcs_bottom_up_value(mat, i - 1, j), lcs_bottom_up_value(mat, i, j - 1));
         }
     }
 
@@ -41,7 +41,7 @@ vector<int> lcs(vector<int> a, vector<int> b) {
     while (i >= 0 && j >= 0) {
         // cout << "[debug]" << "; ";
         // cout << "i = " << i << "; ";
-        // cout << "j = " << j << ";";
+        // cout << "j = " << j << "; ";
         // cout << "a[i] = " << a[i] << "; ";
         // cout << "b[j] = " << b[j] << "; ";
         // cout << endl;
@@ -52,11 +52,83 @@ vector<int> lcs(vector<int> a, vector<int> b) {
             j--;
         }
         else {
-            auto option01 = value(mat, i - 1, j);
-            auto option02 = value(mat, i, j - 1);
+            auto option01 = lcs_bottom_up_value(mat, i - 1, j);
+            auto option02 = lcs_bottom_up_value(mat, i, j - 1);
 
             if (option01 > option02) i--;
             else j--;
+        }
+    }
+
+    return result;
+}
+
+
+
+int _lcs_recursive(const vector<int> &a, const vector<int> &b, size_t i, size_t j, vector<vector<int>> &dp) {
+    if (i >= a.size() || j >= b.size()) return 0;
+
+    if (dp[i][j] == -1) {
+        if (a[i] == b[j]) {
+            dp[i][j] = _lcs_recursive(a, b, i + 1, j + 1, dp) + 1;
+        }
+        else {
+            dp[i][j] = max(
+                _lcs_recursive(a, b, i + 1, j, dp),
+                _lcs_recursive(a, b, i, j + 1, dp)
+            );
+        }
+    }
+
+    // cout << "[debug]" << "; ";
+    // cout << "i = " << i << "; ";
+    // cout << "j = " << j << "; ";
+    // cout << "dp[i][j] = " << dp[i][j] << "; ";
+    // cout << endl;
+    
+    return dp[i][j];
+}
+
+
+vector<int> lcs_recursive(vector<int> a, vector<int> b) {
+    auto dp = vector<vector<int>>(a.size(), vector<int>(b.size(), -1));
+
+    _lcs_recursive(a, b, 0, 0, dp);
+
+    // for (size_t i = 0; i < a.size(); i++) {
+    //     for (size_t j = 0; j < b.size(); j++) {
+    //         cout << dp[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    size_t i = 0;
+    size_t j = 0;
+    vector<int> result;
+
+    while (i < a.size() && j < b.size()) {
+        // cout << "[debug]" << "; ";
+        // cout << "i = " << i << "; ";
+        // cout << "j = " << j << "; ";
+        // cout << "a[i] = " << a[i] << "; ";
+        // cout << "b[j] = " << b[j] << "; ";
+        // cout << endl;
+
+        if (a[i] == b[j]) {
+            result.push_back(a[i]);
+            i++;
+            j++;
+        }
+        else {
+            auto option01 = i + 1 < a.size()
+                            ? dp[i + 1][j]
+                            : -1;
+            auto option02 = j + 1 < b.size()
+                            ? dp[i][j + 1]
+                            : -1;
+
+            if (option01 > option02) i++;
+            else j++;
         }
     }
 
@@ -84,7 +156,7 @@ int main(void) {
     }
     // cout << endl;
 
-    vector<int> result = lcs(A, B);
+    vector<int> result = lcs_recursive(A, B);
 
     if (!result.empty()) {
         cout << result[0];
