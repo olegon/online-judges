83c9a49111c01991339ba113edb21a64c3620532
Leandro Gonçalves de Oliveira <olegon.main@gmail.com>
2023-06-19 20:14:51 -0300

    LeetCode
    - updating shell script to support clang++ and c++17 because leetcode
      uses it: https://support.leetcode.com/hc/en-us/articles/360011833974-What-are-the-environments-for-the-programming-languages-

--- leetcode/two-sum/compile-and-run.sh
+++ leetcode/two-sum/compile-and-run.sh
@@ -2,5 +2,4 @@
 
 set -x
 
-g++ main.cpp -W -Wall -std=c++14 -O3 && for f in input*.txt; do cat "$f" | ./a.out; done;
-
+clang++ main.cpp -W -Wall -std=c++17 -O2 && for f in input*.txt; do cat "$f" | ./a.out; done;
