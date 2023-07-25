3e6929766586e9a57b7d821520e04ce8aee143a9
Leandro Gonçalves de Oliveira <olegon.main@gmail.com>
2023-07-24 21:17:35 -0300

    LeetCode: 143. Reorder List
    - adding "honest" and deque solutions;

--- leetcode/problems/reorder-list/main.cpp
+++ leetcode/problems/reorder-list/main.cpp
@@ -18,6 +18,14 @@ struct ListNode {
 class Solution {
 public:
     void reorderList(ListNode* head) {
+        HonestSolution s;
+        s.reorderList(head);
+    }
+};
+
+class HonestSolution {
+public:
+    void reorderList(ListNode* head) {
         int size = getSize(head);
 
         ListNode *firstSegment = head;
@@ -85,6 +93,39 @@ public:
         }
 
         return prev;
+    }
+};
+
+class DequeSolution {
+public:
+    void reorderList(ListNode* head) {
+        ListNode *node;
+        deque<ListNode*> d;
+
+        node = head;
+        while (node != NULL) {
+            d.push_back(node);
+            node = node->next;
+        }
+
+        node = d.front();
+        d.pop_front();
+        
+        while (true) {
+            if (d.empty()) break;
+
+            node->next = d.back();
+            d.pop_back();
+            node = node->next;
+
+            if (d.empty()) break;
+
+            node->next = d.front();
+            d.pop_front();
+            node = node->next;
+        }
+
+        node->next = NULL;
     }
 };
 
